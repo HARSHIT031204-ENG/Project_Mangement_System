f31b76f3aa5fd37eaedf1d71938e47692ddcb330
harshitgarg <gargh2120@gmail.com>
2025-10-19 15:41:07 +0530

    add fetures

--- src/controllers/healthController.js
+++ src/controllers/healthController.js
@@ -13,6 +13,7 @@ import { asynchandler } from "../utils/async-handler.js";
 
 export const healthController = asynchandler(async (req, res, next) => {
   const {email} = req.body
+  
   res
     .status(200)
     .json(new ApiResponse(200, { message: "server is still running ", email }));
