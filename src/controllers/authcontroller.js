cdcfa4a86292ffc80916ca8422b8fcfc5b09bf34
harshitgarg <gargh2120@gmail.com>
2025-10-25 12:21:59 +0530

    adding fetures

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -1,71 +1,132 @@
 import { User } from "../models/user.model.js";
-import {ApiError} from '../utils/api-error.js'
-import {ApiResponse} from '../utils/api-response.js'
-import {asynchandler} from '../utils/async-handler.js'
-import SendMail from '../utils/mail.js'
+import { ApiError } from "../utils/api-error.js";
+import { ApiResponse } from "../utils/api-response.js";
+import { asynchandler } from "../utils/async-handler.js";
+import SendMail from "../utils/mail.js";
 
-const generate_Access_Refresh_Token = async(usreId) => {
-    
-    try {
-        const user = User.findOne({usreId})
-        const AccesToken = user.generate_access_token()
-        const RefreshToken = user.generate_refresh_token()
+const generate_Access_Refresh_Token = async (usreId) => {
+  try {
+    const user = await User.findOne(usreId);
+    console.log("user is ", user);
 
+    const AccesToken = await user.generate_access_token();
+    const RefreshToken = await user.generate_refresh_token();
 
-        user.refreshtoken = RefreshToken
-        await user.save({validateBeforeSave : false})
-        return {AccesToken, RefreshToken}
-        
-    } catch (error) {
-        console.log("Error in accesing tokens :", error);
-        throw new ApiError(400, {message : "problem in accessing tokens"}, [])
-        
-    }
-}
+    user.refreshtoken = RefreshToken;
+    await user.save({ validateBeforeSave: false });
+    return { AccesToken, RefreshToken };
+  } catch (error) {
+    console.log("Error in accesing tokens :", error);
+    throw new ApiError(400, { message: "problem in accessing tokens" }, []);
+  }
+};
 export const Rgisteruser = asynchandler(async (req, res) => {
-    const{ email, password, username, role } = req.body
-    
-  const exisusre = User.findOne({
-        email
-    })
-    
-    if(exisusre) throw new ApiError(409, {message: "user exist already !"}, [])
+  const { email, username } = req.validation;
+  const { password, role } = req.body;
+  // console.log(password);
 
-    const user = await User.create({
-        email, 
-        password, 
-        username, 
-        isEmailverified : false
-    })
+  const exisusre = await User.findOne({
+    email,
+  });
 
+  if (exisusre) throw new ApiError(409, "user exist already !");
 
-    console.log("usre is", user);
-    
-    const {unhashedtoken, hashedtoken, tokenexpiry} = user.generate_temporaray_token()
+  const user = await User.create({
+    email,
+    password,
+    username,
+    isEmailverified: false,
+  });
 
-    const {AccesToken, RefreshToken} = generate_Access_Refresh_Token()
+  // console.log("usre is", user);
 
-    user.emailverificationToken = hashedtoken
-    user.emailverificationexpiry = tokenexpiry
+  const { unhashedtoken, hashedtoken, tokenexpiry } =
+    user.generate_temporaray_token();
 
-    await user.save({validateBeforeSave: false})
+  const { AccesToken, RefreshToken } = await generate_Access_Refresh_Token(
+    user._id,
+  );
 
-    await SendMail({
-        email : user?.email,
-        subject : "Please verify your mail ",
-        mailgenContent: EmailverificationMailgen(user.username, 
-            `${req.protocol}://${req.get("host")}/api/v1/user/verify-email${unhashedtoken}`
-        )
-    })
+  user.emailverificationToken = hashedtoken;
+  user.emailverificationexpiry = tokenexpiry;
 
-    
-   const createdUser =  await User
-        .findById(user._id)
-        .select("-password -refreshtoken -emailverificationToken -emailverificationexpiry")
+  await user.save({ validateBeforeSave: false });
 
-    if(!createdUser) {
-        throw new ApiError(500, "something went wrong while registering a user")
-    }
+  await SendMail({
+    email: user?.email,
+    subject: "Please verify your mail ",
+    mailgenContent: EmailverificationMailgen(
+      user.username,
+      `${req.protocol}://${req.get("host")}/api/v1/user/verify-email${unhashedtoken}`,
+    ),
+  });
 
-    return res.status(200).json( new ApiResponse(200, {user : createdUser}, "user registerd successfull as well as verification successfull"))
-})
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshtoken -emailverificationToken -emailverificationexpiry",
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "something went wrong while registering a user");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        { user: createdUser },
+        "user registerd successfull as well as verification successfull",
+      ),
+    );
+});
+
+export const loginUser = asynchandler(async (req, res) => {
+  const { email, password } = await req.body;
+
+  if (!email || !password) {
+    throw new ApiError(400, "email or password requierd!");
+  }
+
+  const existUser = await User.findOne({ email });
+  if (!existUser) {
+    throw new ApiError(400, "user doesn't exist!");
+  }
+
+  const { AccesToken, refreshtoken } = await generate_Access_Refresh_Token(
+    existUser._id,
+  );
+
+  const isPasswordCorrect = await existUser.isPasswordCorrect(password);
+
+  //    console.log(ispasswordcorrect);
+  console.log("User hashed password:", existUser.password);
+console.log("Entered password:", password);
+console.log("Compare result:", isPasswordCorrect);
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "password incorrect!");
+  }
+
+  const loginuser = await existUser
+    .findOne({ email })
+    .select(
+      "-password -refreshtoken -emailverificationToken -emailverificationexpiry",
+    );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  res
+    .send(200)
+    .cookie("acces tokren", AccesToken, options)
+    .cookie("refresh tokren", refreshtoken, options)
+    .json(
+      new ApiResponse(200, {
+        message: "Logged in successful",
+        loggeduser: loginuser,
+        refreshtoken: refreshtoken,
+        AccesToken: AccesToken,
+      }),
+    );
+});
