c68b446942a9fe0a82511414a9aa876207064cc0
harshitgarg <gargh2120@gmail.com>
2025-10-25 12:23:31 +0530

    adding fetures

--- src/middlewares/validator.middleware.js
+++ src/middlewares/validator.middleware.js
@@ -1,7 +1,32 @@
-import { validationResult } from "express-validator";
-import {ApiError} from "../utils/api-error.js"
+import { z } from "zod";
+import { ApiError } from "../utils/api-error.js";
+const uservalid_schema = z.object({
+  username: z.preprocess(
+    (val) => (typeof val == String ? val.trim().toLowerCase() : val),
+    z
+      .string({ message: "enter in a string formatt." })
+      .min(3, { message: "atleast three character ,ust be exist " })
+      .max(20, { message: " not more than 20 character " }),
+  ),
 
+  email: z.preprocess(
+    (val) => (typeof val == String ? val.trim().toLowerCase() : val),
+    z.email({ message: "email not valid !" }),
+  ),
+});
 
-export const validate = (req, res, next) => {
-    return 
-}
+export const validation = (req, res, next) => {
+  const { email, username } = req.body;
+  // console.log(email);
+
+  const result = uservalid_schema.safeParse({ username, email });
+  // console.log("Result data: ", result);
+
+  if (!result.success) {
+    console.log(result.error.format());
+    throw new ApiError(400, "Validation Error.", result.error.format());
+  }
+
+  req.validation = result.data;
+  next();
+};
