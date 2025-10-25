e5f9a640003ca21392a6d84c77a73341d5b12d06
harshitgarg <gargh2120@gmail.com>
2025-10-25 12:24:29 +0530

    adding fetures

--- src/routes/authrouter.js
+++ src/routes/authrouter.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
-import { Rgisteruser } from "../controllers/authcontroller.js"
-const router = Router()
+import { loginUser, Rgisteruser } from "../controllers/authcontroller.js";
+import { validation } from "../middlewares/validator.middleware.js";
+const router = Router();
 
-router.route("/register").post(Rgisteruser)
-
-export default router
+router.route("/register").post( validation, Rgisteruser);
+router.route("/login").get(loginUser)
+export default router;
