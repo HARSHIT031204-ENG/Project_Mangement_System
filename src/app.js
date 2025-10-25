45578d050f875da6402417b82415c1f8ba7a9c0d
harshitgarg <gargh2120@gmail.com>
2025-10-25 12:24:49 +0530

    adding fetures

--- src/app.js
+++ src/app.js
@@ -2,23 +2,24 @@ import express from "express";
 import cors from "cors"
 import healthcheckroute from "./routes/healthroutes.js"
 import authrouter from "./routes/authrouter.js"
-// import AuthRouter from "./routes/authrouter.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // basic configuration
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended : true, limit : "16kb"}))
 app.use(express.static("public"))
-
+app.use(cookieParser())
 // cors 
 app.use(cors({
     origin: process.env.CORS_ORIGIN?.split(",") || "http://localhost:5173",
     credentials: true,
-    //methods: ["GET", "POST", "DELETE", "HEAD", "PUT", "PATCH"],
-    //allowedHeaders: ["Authorization", "Content-Type"]
+    methods: ["GET", "POST", "DELETE", "HEAD", "PUT", "PATCH"],
+    allowedHeaders: ["Authorization", "Content-Type"]
 }))
 
 
-app.use("/api/v1", authrouter);
+app.use("/api/v1", authrouter, healthcheckroute);
+
 
 export default app;
