82adec434602c53d34df2fed73749bbc27137a5f
harshitgarg <gargh2120@gmail.com>
2025-10-31 14:41:12 +0530

    adding features

--- src/controllers/authcontroller.js
+++ src/controllers/authcontroller.js
@@ -23,7 +23,6 @@ const generate_Access_Refresh_Token = async (usreId) => {
   }
 };
 
-
 export const Rgisteruser = asynchandler(async (req, res) => {
   const { email, username } = req.validation;
   const { password, role } = req.body;
